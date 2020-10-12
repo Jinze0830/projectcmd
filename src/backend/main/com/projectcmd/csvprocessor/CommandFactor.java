0ff664d451065bf7ca380261ea899e54852ec1a9
Jinze0830 <jinze1990@gmail.com>
2020-10-12 13:43:59 -0400

    fix format again

--- src/backend/main/com/projectcmd/csvprocessor/CommandFactor.java
+++ src/backend/main/com/projectcmd/csvprocessor/CommandFactor.java
@@ -1,59 +1,31 @@
 package backend.main.com.projectcmd.csvprocessor;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommandFactor {
-    public static byte[] getHexStr(String command, String format, String content) {
+    public static byte[] getHexStr(String command, String format, String content) throws UnsupportedEncodingException {
         String message = "";
         message = command == null || command.equals("") ? message : command;
         message = format == null || format.equals("") ? message : message + "," + format;
         message = content == null || content.equals("") ? message : message + "," + content;
 
-        char[] arr = message.toCharArray();
-        String hexStr = "";
-        for(char c: arr) {
-            String hexString = Integer.toHexString(c);
-            hexStr = hexString;
-        }
+        message = message + '\r';
 
-        hexStr = hexStr + "0d";
-
-
-        return hexStringToByteArray(hexStr);
+        return message.getBytes("UTF8");
     }
 
     public static List<byte[]> getHexStrs(String command, String format, List<String> content) {
         List<byte[]> hexStrs = new ArrayList<>();
-        content.forEach(cur -> hexStrs.add(CommandFactor.getHexStr(command, format, cur)));
+        content.forEach(cur -> {
+            try {
+                hexStrs.add(CommandFactor.getHexStr(command, format, cur));
+            } catch (UnsupportedEncodingException e) {
+                throw new UnsupportedOperationException();
+            }
+        });
+
         return hexStrs;
-    }
-
-    public static byte[] hexStringToByteArray(String hexString) {
-        if (hexString.length() % 2 == 1) {
-            throw new IllegalArgumentException(
-                    "Invalid hexadecimal String supplied.");
-        }
-
-        byte[] bytes = new byte[hexString.length() / 2];
-        for (int i = 0; i < hexString.length(); i += 2) {
-            bytes[i / 2] = hexToByte(hexString.substring(i, i + 2));
-        }
-        return bytes;
-    }
-
-    public static byte hexToByte(String hexString) {
-        int firstDigit = toDigit(hexString.charAt(0));
-        int secondDigit = toDigit(hexString.charAt(1));
-        return (byte) ((firstDigit << 4) + secondDigit);
-    }
-
-    private static int toDigit(char hexChar) {
-        int digit = Character.digit(hexChar, 16);
-        if(digit == -1) {
-            throw new IllegalArgumentException(
-                    "Invalid Hexadecimal Character: "+ hexChar);
-        }
-        return digit;
     }
 }
--- src/backend/test/com/projectcmd/csvprocessor/CommandFactorTest.java
+++ src/backend/test/com/projectcmd/csvprocessor/CommandFactorTest.java
@@ -3,12 +3,14 @@ package backend.test.com.projectcmd.csvprocessor;
 import backend.main.com.projectcmd.csvprocessor.CommandFactor;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+
 import static org.junit.Assert.assertEquals;
 
 public class CommandFactorTest {
 
     @Test
-    public void getHexStrTest() {
+    public void getHexStrTest() throws UnsupportedEncodingException {
         assertEquals("42482c322c414243440d",
                 CommandFactor.getHexStr("BH", "2", "ABCD"));
     }
